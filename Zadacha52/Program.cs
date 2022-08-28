2c4bf322ff3749aef2d48227a28c38d4e0e58e30
Anna <anyutka901990@mail.ru>
2022-08-28 22:49:50 +0300

    Zadacha 52

---
+++ Zadacha52/Program.cs
@@ -0,0 +1,33 @@
+﻿//  Задайте двумерный массив из целых чисел. 
+//  Найдите среднее арифметическое элементов в каждом столбце.
+
+
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] mass = new int[m, n];
+Random rand = new Random();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        mass[i, j] = new Random().Next(1, 10);
+        Console.Write(mass[i, j] + " ");
+    }
+    Console.WriteLine();
+
+}
+double result = 0;
+for (int i = 0; i < m; i++)
+{
+    
+  for (int j = 0; j < m; j++)
+  {
+    result += mass[j, i];
+  }
+  result = Math.Round(result / m, 1);
+  Console.Write(result + "; ");
+}
+
+
